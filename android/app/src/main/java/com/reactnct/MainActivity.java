4e7d041aa449067ea37319d441a5024717e809a7
Rashmi-9514 <rashmi@clevertap.com>
2022-06-23 14:29:24 +0530

    Initial commit

---
+++ android/app/src/main/java/com/reactnct/MainActivity.java
@@ -0,0 +1,36 @@
+package com.reactnct;
+
+
+
+import android.os.Bundle;
+
+import com.facebook.react.ReactActivity;
+
+
+import java.util.List;
+
+
+public class MainActivity<packages> extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+ 
+  protected String getMainComponentName() {
+    return "finaldemo";
+  }
+
+  protected void onCreate(Bundle savedInstanceState) {
+
+
+
+    super.onCreate(savedInstanceState);
+
+
+  }
+
+
+
+}
